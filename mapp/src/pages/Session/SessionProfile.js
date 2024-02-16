4e75f53ac8666c487bf7702c2a36acfba20d5f80
Holly Boaz <hollykaboaz@gmail.com>
2024-02-16 01:42:34 -0500

    Adding Scanning Button

--- mapp/src/pages/Session/SessionProfile.js
+++ mapp/src/pages/Session/SessionProfile.js
@@ -1,23 +1,24 @@
-import React, {useEffect, useState} from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {Html5QrcodeScanner} from "html5-qrcode";
-import {faQrcode, faUserGroup, faUserXmark} from "@fortawesome/free-solid-svg-icons";
-import SessionsAttendanceTable from '../../tables/SessionsAttendanceTable';
+import React, {useEffect, useState} from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {Html5QrcodeScanner} from 'html5-qrcode'
+import {faQrcode} from '@fortawesome/free-solid-svg-icons'
+import SessionsAttendanceTable from '../../tables/SessionsAttendanceTable'
+import SecondaryButton from '../../components/SecondaryButton'
 
 function SessionProfile({session}) {
 
-    const [scanResult, setScanResult] = useState(null);
-    const [manualSerialNumber, setManualSerialNumber] = useState('');
+    const [scanResult, setScanResult] = useState(null)
+    const [manualSerialNumber, setManualSerialNumber] = useState('')
 
     useEffect(() => {
-        const scanner = new Html5QrcodeScanner('reader', {
-            qrbox: {
-                width: 200,
-                height: 200,
-            }, fps: 5
-        })
+        // const scanner = new Html5QrcodeScanner('reader', {
+        //     qrbox: {
+        //         width: 200,
+        //         height: 200,
+        //     }, fps: 5
+        // })
 
-        scanner.render(success, error);
+        // scanner.render(success, error)
 
         function success(result) {
             setScanResult(result)
@@ -26,7 +27,7 @@ function SessionProfile({session}) {
         function error(err) {
             console.warn(err)
         }
-    }, []);
+    }, [])
 
     const data = [
         {
@@ -49,34 +50,23 @@ function SessionProfile({session}) {
     ]
 
     return (
-        <div className='mx-12 py-4'>
-            <div className='grid grid-cols-2 gap-2'>
-                <div className='grid grid-cols-2 h-fit justify-stretch gap-2'>
-                    <div className='flex flex-col text-green-900 bg-green-200 px-4 py-4 rounded-3xl'>
-                        <FontAwesomeIcon className='h-6 w-6 pb-6' icon={faUserGroup}/>
-                        <div className='text-5xl font-semibold'>23</div>
-                        <div className='font-light'>Present</div>
-                    </div>
-
-                    <div className='flex flex-col text-red-900 bg-red-200 px-4 py-4 rounded-3xl'>
-                        <FontAwesomeIcon className='h-6 w-6 pb-6' icon={faUserXmark}/>
-                        <div className='text-5xl font-semibold'>5</div>
-                        <div className='font-light'>Absent</div>
-                    </div>
-                </div>
-
-                <div className='col-span-1 row-span-1 flex flex-col text-gray-900 bg-gray-200 px-4 py-4 rounded-3xl'>
-                    <FontAwesomeIcon className='h-6 w-6 pb-6' icon={faQrcode}/>
-                    <div className='font-light'>Scan in Students</div>
-                    {scanResult ?
-                        <div> Success: <a className='text-green-500' href={scanResult}>{scanResult}</a></div> :
-                        <div id="reader"></div>}
-                </div>
+        <div className="mx-12 py-4">
+            {/*<div className="grid grid-cols-2 gap-2">*/}
+            {/*    <div className="col-span-1 row-span-1 flex flex-col text-gray-900 bg-gray-200 px-4 py-4 rounded-3xl">*/}
+            {/*        <FontAwesomeIcon className="h-6 w-6 pb-6" icon={faQrcode}/>*/}
+            {/*        <div className="font-light">Scan in Students</div>*/}
+            {/*        {scanResult ?*/}
+            {/*            <div> Success: <a className="text-green-500" href={scanResult}>{scanResult}</a></div> :*/}
+            {/*            <div id="reader"></div>}*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <div className="flex justify-between">
+                <div className="text-2xl font-medium"> Attendance Report</div>
+                <button className='bg-black text-sm text-white rounded py-2 px-4'>Start Scanning</button>
             </div>
-            <div className='text-2xl font-medium mt-12'> Attendance Report</div>
             <SessionsAttendanceTable data={data}/>
         </div>
-    );
+    )
 }
 
-export default SessionProfile;
+export default SessionProfile
