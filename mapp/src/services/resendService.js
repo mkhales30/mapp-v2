acffc13ec79d303bd81dc5d52898100e0c340994
Khales Rahman <mrahman10@ggc.edu>
2024-04-23 11:43:50 -0400

    Updates to website tab UI

--- mapp/src/services/resendService.js
+++ mapp/src/services/resendService.js
@@ -1,24 +1,24 @@
-const { Resend } = require('resend');
+// const { Resend } = require('resend');
 
-const resend = new Resend('re_G3pCqaVd_Nw3e8zrNM22XzXMkufTcHMTi');
+// const resend = new Resend('re_G3pCqaVd_Nw3e8zrNM22XzXMkufTcHMTi');
 
-export const sendEmail = async (recipients, subject, body) => {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'tsm_tin@yahoo.com',
-      to: recipients,
-      subject: subject,
-      html: `<h3>${subject}</h3><p>${body}</p>`,
-    });
+// export const sendEmail = async (recipients, subject, body) => {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: 'tsm_tin@yahoo.com',
+//       to: recipients,
+//       subject: subject,
+//       html: `<h3>${subject}</h3><p>${body}</p>`,
+//     });
 
-    if (error) {
-      console.error('Error sending email:', error);
-      throw error;
-    }
+//     if (error) {
+//       console.error('Error sending email:', error);
+//       throw error;
+//     }
 
-    console.log('Email sent successfully:', data);
-  } catch (error) {
-    console.error('Error sending email:', error);
-    throw error;
-  }
-};
+//     console.log('Email sent successfully:', data);
+//   } catch (error) {
+//     console.error('Error sending email:', error);
+//     throw error;
+//   }
+// };
